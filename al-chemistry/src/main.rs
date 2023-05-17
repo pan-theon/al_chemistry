1128b78de9bf5ad2720bc3d0fe1cc81757f33d6d
Boris Milchenko <borismilchenko@gmail.com>
2023-05-17 19:59:50 +0300

    Removed: get_periodic_table() from main.rs

--- al-chemistry/src/main.rs
+++ al-chemistry/src/main.rs
@@ -19,24 +19,3 @@ fn main() {
     assert!(alohco3.class == SubstanceClass::Salt);
     assert!(alohco3.content.get("OH").is_some());
 }
-
-fn get_periodic_table() -> Result<HashMap<String, Element>, &'static str> {
-    extern crate serde;
-    extern crate serde_json;
-
-    use std::fs;
-
-    let table_file = fs::read_to_string(".applications/periodic_table.json");
-    let table: String;
-    match table_file {
-        Err(_) => return Err(&"Unable to open file '.applications/periodic_table.json'"),
-        Ok(t) => table = t,
-    };
-
-    let table = serde_json::from_str(&table);
-    match table {
-        Err(_) => Err(&"Periodic table is inconsistent
-            ('.applications/periodic_table.json')"),
-        Ok(res) => Ok(res),
-    }
-}
